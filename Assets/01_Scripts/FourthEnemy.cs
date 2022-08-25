d31a053c024e47d46b3193d83327d6a5a5c29631
ms9876 <ggm.s220111@ggh.goe.go.kr>
2022-08-26 02:30:29 +0900

    8월 26일

---
+++ Assets/01_Scripts/FourthEnemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourthEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Enemy1, Enemy2;
+
+    private void Move()
+    {
+        
+    }
+}
---
+++ Assets/01_Scripts/ThirdScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdScene : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject start_btn;
+    [SerializeField]
+    private GameObject back_btn;
+
+    [SerializeField]
+    private GameObject second_Scene, third_Scene, fourth_Scene;
+
+
+    public void OnClickStart()
+    {
+        third_Scene.SetActive(false);
+        fourth_Scene.SetActive(true);
+    }
+
+    public void OnClickBack()
+    {
+        third_Scene.SetActive(false);
+        second_Scene.SetActive(true);
+    }
+}
